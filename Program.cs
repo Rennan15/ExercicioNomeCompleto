c4eb5debdf6c87281fe691efdc5fce0354e49748
Rennan Monteiro <rennan.monteiro@etec.sp.gov.br>
2023-09-04 10:01:14 -0300

    Feito

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿string Nome, Sobrenome;
+
+Console.Write("Escreva seu primeiro Nome:");
+Nome=Console.ReadLine();
+
+Console.Write("Escreva seu sobrenome:");
+Sobrenome=Console.ReadLine();
+
+Console.WriteLine($"{Nome} {Sobrenome}");
+Console.WriteLine($"{Sobrenome.ToUpper()}, {Nome}");
